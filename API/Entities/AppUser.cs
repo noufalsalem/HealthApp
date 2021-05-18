caf38fba8c2571f09bf65053f4990df00a5b537d
noufalsalem <noufhalsalem@gmail.com>
2021-05-18 12:59:07 +0300

    sec8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser //named AppUser to specify user type
@@ -7,12 +11,25 @@ namespace API.Entities
         /*For properties, recommended use same labels 
         so entity framework recognizes it as the right field.**/
         public int Id { get; set; } //Identity property
-
         public string UserName { get; set; } //UserName property
-        
         public byte[] PasswordHash {get; set;}
-
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string ExperiencedWith { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
 
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
+
+        
     }
 }
